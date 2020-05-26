306ca377ad443a2d2e950eee94010b87cf69ed96
Nicolas Toulemont <n.toulemont@gmail.com>
2020-05-26 08:25:18 +0200

    added basic example

--- example/index.tsx
+++ example/index.tsx
@@ -1,14 +1,55 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../.';
+import { useFetch } from '../.';
 
-const App = () => {
+function Input() {
+  const [input, setInput] = React.useState('');
+
+  const API = 'https://pokeapi.co/api/v2/';
+  const { response, loading, fetchy } = useFetch(API.concat(input), {});
+
+  function handleClick() {
+    fetchy();
+  }
+
   return (
-    <div>
-      <Thing />
+    <>
+      <div style={{ marginBottom: '20px' }}>
+        <input
+          name="input"
+          value={input}
+          onChange={e => setInput(e.target.value)}
+        />
+        <button onClick={handleClick}>Fetch</button>
+      </div>
+      <div style={{ maxWidth: '50%', margin: '0 auto' }}>
+        {loading ? (
+          <div>Loading...</div>
+        ) : (
+          response && <div>{JSON.stringify(response, null, 4)}</div>
+        )}
+      </div>
+    </>
+  );
+}
+
+export default function App() {
+  const [showInput, setShowInput] = React.useState(true);
+  return (
+    <div
+      className="App"
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        width: '50%',
+        margin: '0 auto',
+      }}
+    >
+      <button onClick={() => setShowInput(!showInput)}>Handle Input</button>
+      {showInput && <Input />}
     </div>
   );
-};
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
